b0f9f450f5a56bdd77d1f4147a2acde487723900
Amin Rahmati <amin.nima3490@gmail.com>
2023-02-13 16:02:10 +0330

    open id added

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -9,11 +10,11 @@ namespace IdentityServer
 {
     public static class Config
     {
-        public static IEnumerable<IdentityResource> IdentityResources =>
-            new IdentityResource[]
-            {
-                new IdentityResources.OpenId()
-            };
+        public static IEnumerable<IdentityResource> IdentityResources => new List<IdentityResource>
+           {
+               new IdentityResources.OpenId(),
+               new IdentityResources.Profile(),
+          };
 
         public static IEnumerable<ApiScope> ApiScopes =>
                new List<ApiScope>
@@ -89,7 +90,26 @@ namespace IdentityServer
 
                     // scopes that client has access to
                     AllowedScopes = { "fullaccess" }
-                }
+                },
+                  new Client
+        {
+            ClientId = "mvc",
+            ClientSecrets = { new Secret("secret".Sha256()) },
+
+            AllowedGrantTypes = GrantTypes.Code,
+
+            // where to redirect to after login
+            RedirectUris = { "https://localhost:5002/signin-oidc" },
+
+            // where to redirect to after logout
+            PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+            AllowedScopes = new List<string>
+            {
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile
+            }
+        }
             };
     }
 }
